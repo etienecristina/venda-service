46959e18327781d5affd365844f7bb98a256ed01
etienecristina <etiene.scristina@gmail.com>
2025-07-31 15:35:42 -0300

    teste

--- src/main/java/br/com/fiap/challenge/vendas/model/VendaModel.java
+++ src/main/java/br/com/fiap/challenge/vendas/model/VendaModel.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class VendaModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
